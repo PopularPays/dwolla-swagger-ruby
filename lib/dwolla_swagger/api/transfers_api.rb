e067ce5a253ec56612f304fbc550dcfaec2f05df
Ian Pierce <ian@popularpays.com>
2021-03-19 10:19:33 -0500

    added logging

--- lib/dwolla_swagger/api/transfers_api.rb
+++ lib/dwolla_swagger/api/transfers_api.rb
@@ -143,6 +143,10 @@ module DwollaSwagger
 
       response = Swagger::Request.new(:POST, path, {:params => query_params, :headers => header_params, :form_params => form_params, :body => post_body, :auth_names => @auth_names}).make
 
+      puts "CREATED TRANSFER -- DWOLLA SWAGGER"
+      puts response.body
+      puts response.headers
+
       response.code == 201 ? obj = response.headers['Location'] : (obj = Unit.new() and obj.build_from_hash(response.body))
 
     end
